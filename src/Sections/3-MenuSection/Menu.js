f25e8809321f595844aa71df58139c41bbc8d5be
Jeko92 <jkhachidze92@gmail.com>
2023-05-25 11:12:19 +0200

    code Refaktor

--- src/Sections/3-MenuSection/Menu.js
+++ src/Sections/3-MenuSection/Menu.js
@@ -7,6 +7,7 @@ import {
   IoIosPizza,
   IoIosMic,
 } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
   return (
@@ -23,35 +24,52 @@ export default function Menu() {
           <h2 className="assortment-section-header">Choose your Menu</h2>
           <ul className="assortment-section-items">
             <li key={Math.random()} className="assortment-section-item">
-              <a href="./menu.html">
+              <Link to="/menu">
                 Breakfast
                 <IoIosRestaurant size={40} />
-              </a>
+              </Link>
+              {/* <a href="./menu.html">
+                Breakfast
+                <IoIosRestaurant size={40} />
+              </a> */}
             </li>
             <li key={Math.random()} className="assortment-section-item">
-              <a href="./menu.html">
+              <Link to="/menu">
                 Lunch <IoIosPint size={40} />
-              </a>
+              </Link>
+              {/* <a href="./menu.html">
+                Lunch <IoIosPint size={40} />
+              </a> */}
             </li>
             <li key={Math.random()} className="assortment-section-item">
-              <a href="./menu.html">
+              <Link to="/menu">
                 Dinner <IoIosPizza size={40} />
-              </a>
+              </Link>
+              {/* <a href="./menu.html">
+                Dinner <IoIosPizza size={40} />
+              </a> */}
             </li>
             <li key={Math.random()} className="assortment-section-item">
-              <a href="./menu.html">
+              <Link to="/menu">
                 Bar <IoIosWine size={40} />
-              </a>
+              </Link>
+              {/* <a href="./menu.html">
+                Bar <IoIosWine size={40} />
+              </a> */}
             </li>
             <li key={Math.random()} className="assortment-section-item">
-              <a href="./menu.html">
+              <Link to="/menu">
                 Events <IoIosMic size={40} />
-              </a>
+              </Link>
+              {/* <a href="./menu.html">
+                Events <IoIosMic size={40} />
+              </a> */}
             </li>
           </ul>
-          <a className="assortment-btn" role="button" href="./menu.html">
+          <Link to="/menu" className="assortment-btn" role="button">
             Check Menu details
-          </a>
+          </Link>
+          {/*  */}
         </div>
       </div>
     </section>
--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -3,15 +3,15 @@ import "./component-styles/BookingForm.css";
 export default function BookingForm() {
   return (
     <form>
-      <label className="fNameLabel" for="fname">
+      <label className="fNameLabel" htmlFor="fname">
         First name:
       </label>
       <input type="text" id="fname" name="fname" required />
-      <label className="lNameLabel" for="lname">
+      <label className="lNameLabel" htmlFor="lname">
         Last name:
       </label>
       <input type="text" id="lname" name="lname" required />
-      <label className="emailLabel" for="email">
+      <label className="emailLabel" htmlFor="email">
         Email:
       </label>
       <input
@@ -19,10 +19,10 @@ export default function BookingForm() {
         placeholder="ex: johndoe@mail.com"
         id="email"
         required
-        autocomplete="email"
+        autoComplete="email"
       />
 
-      <label for="nGuests">Number of guests:</label>
+      <label htmlFor="nGuests">Number of guests:</label>
       <input
         type="number"
         min="1"
@@ -32,19 +32,19 @@ export default function BookingForm() {
         required
       />
 
-      <label for="phone">Phone Number:</label>
+      <label htmlFor="phone">Phone Number:</label>
       <input
         type="tel"
         id="phone"
         pattern="+[0-9]{2}-[0-9]{4}-[0-9]{7}"
         placeholder="+49-1590-1234565"
-        autocomplete="tel-local"
+        autoComplete="tel-local"
       />
-      <label for="date">Select Month:</label>
+      <label htmlFor="date">Select Month:</label>
       <input type="date" id="date" required />
       {/* <label for="time">Select Time:</label>
           <input type="time" id="time" required /> */}
-      <label for="time">Select Time</label>
+      <label htmlFor="time">Select Time</label>
       <select name="time" id="time" className="desiredTime">
         <option value="17:00">17:00</option>
         <option value="17:30">17:30</option>
@@ -59,7 +59,7 @@ export default function BookingForm() {
         <option value="22:00">22:00</option>
       </select>
 
-      <label for="occasion">Choose occasion:</label>
+      <label htmlFor="occasion">Choose occasion:</label>
       <select name="occasion" id="occasion">
         <option value="none">None</option>
         <option value="business">Business lunch</option>
@@ -67,14 +67,14 @@ export default function BookingForm() {
         <option value="anniversary">Anniversary</option>
         <option value="other">Other</option>
       </select>
-      <label for="seatingPref">Seating preferences:</label>
+      <label htmlFor="seatingPref">Seating preferences:</label>
       <select name="seatingPref" id="seatingPref">
         <option value="none">None</option>
         <option value="indoors">Indoors</option>
         <option value="outdoor">Outdoors</option>
         <option value="Terrace">Terrace</option>
       </select>
-      <label for="guestComments">Additional Comments:</label>
+      <label htmlFor="guestComments">Additional Comments:</label>
 
       <textarea
         id="guestComments"
