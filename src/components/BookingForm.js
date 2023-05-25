dcbf77325aafd2d05a52386ee96e86a4ab738d23
Jeko92 <jkhachidze92@gmail.com>
2023-05-25 16:37:49 +0200

    Finished, form needs some more work

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,6 +1,7 @@
 import "./component-styles/BookingForm.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+
 export default function BookingForm(props) {
   const [fName, setFName] = useState("");
   const [lName, setLName] = useState("");
--- src/pages/ReservationConfirmationPage/ReservationConfirmation.js
+++ src/pages/ReservationConfirmationPage/ReservationConfirmation.js
@@ -6,20 +6,21 @@ export default function ReservationConfirmation() {
     <div className="reservations-wrapper">
       <span className="backdrop-filter"></span>
       <div className="reservation-summary">
-        <h1>
-          We have received your reservation and are happy to welome you soon{" "}
-          <IoCheckmarkCircleOutline className="reservation-summary-icon" />
-        </h1>
+        <h1>Thank you for your Reservation</h1>
         <h2>Here is a quick summary of your reservation:</h2>
         <div className="reservation-summary__card">
-          <ul>
+          {/* <ul>
             <li key={Math.random()}>Name: </li>
             <li key={Math.random()}>Email: </li>
             <li key={Math.random()}>Date: </li>
             <li key={Math.random()}>Time: </li>
             <li key={Math.random()}>Occasion: </li>
             <li key={Math.random()}>Table at: </li>
-          </ul>
+          </ul> */}
+          <p className="confirmation-message">
+            Booking confirmation has been sent to your email!
+            <IoCheckmarkCircleOutline className="reservation-summary-icon" />
+          </p>
           <h4>
             *Thank you for your reservation and if your plans change, please let
             us know to cancel your reservation
